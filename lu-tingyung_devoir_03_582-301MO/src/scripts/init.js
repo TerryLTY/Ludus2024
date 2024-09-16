df80260ccdd04edff5dab69ad8e1b0befece6f67
TerryLTY <143763387+TerryLTY@users.noreply.github.com>
2024-09-16 14:43:38 -0400

    Delete lu-tingyung_devoir_03_582-301MO directory

--- lu-tingyung_devoir_03_582-301MO/src/scripts/init.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  type: Phaser.AUTO,
-  parent: "canvas-wrapper",
-  width: 800,
-  height: 600,
-  scene: [Accueil, Jeu, CommentJouer, Credits, PartieTerminee, Victoire]
-};
-const game = new Phaser.Game(config);
